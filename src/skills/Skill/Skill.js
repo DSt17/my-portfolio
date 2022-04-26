8914e9ded30d244f9d161cf579bc64dd9a58f86f
DSt17 <dima3771770@gmail.com>
2022-04-26 16:10:02 +0300

    refactor style over sass and mixin

--- src/skills/Skill/Skill.js
+++ src/skills/Skill/Skill.js
@@ -5,7 +5,8 @@ import s from './Skill.module.scss'
 const Skill = (props) => {
     return (
         <div className={s.skill}>
-            <div className={s.icon}></div>
+            <div className={s.icon} style={props.style}>
+            </div>
             <h3>{props.title}</h3>
             <p>
                 {props.description}
--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -3,8 +3,35 @@ import s from './Skills.module.scss'
 import styleContainer from '../common/styles/Container.module.css'
 import Skill from "./Skill/Skill";
 
+import reactIcon from '../assets/img/1126012.png'
+import cssIcon from '../assets/img/CSSicon.png'
+import jsIcon from '../assets/img/JSicon.png'
+import htmlIcon from '../assets/img/HTMLIcon.png'
+import reduxIcon from '../assets/img/reduxIcon.png'
+import typeScriptIcon from '../assets/img/ts.png'
+
 
 const Skills = () => {
+    const htmlIconImg = {
+        backgroundImage: `url(${htmlIcon})`,
+    };
+    const cssIconImg = {
+        backgroundImage: `url(${cssIcon})`,
+    };
+    const jsIconImg = {
+        backgroundImage: `url(${jsIcon})`,
+    };
+
+    const reactIconImg = {
+        backgroundImage: `url(${reactIcon})`,
+    };
+    const typeScriptIc = {
+        backgroundImage: `url(${typeScriptIcon})`,
+    };
+    const reduxIconImg = {
+        backgroundImage: `url(${reduxIcon})`,
+    };
+
 
     return (
         <div className={s.skillsBlock}>
@@ -12,18 +39,34 @@ const Skills = () => {
                 <h2>Skills</h2>
                 <div className={s.skills}>
                     <Skill
-                        title={'JS'}
-                        description={'Le ptry. Lorem Ipsum has been the industry’s standard dummy text.'
-                        }
+                        style={htmlIconImg}
+                        title={'HTML'}
+                        description={'Le ptry. Lorem Ipsum has been the industry’s standard dummy text.'}
                     />
                     <Skill
+                        style={cssIconImg}
                         title={'CSS'}
-                        description={'Lo printin been the industry’s standard dummy text.'
-                        }
+                        description={'Le ptry. Lorem Ipsum has been the industry’s standard dummy text.'}
                     />
                     <Skill
-                        title={'React'}
+                        style={jsIconImg}
+                        title={'JavaScript'}
                         description={'L the pri sdssdsssssd sds dsd sd ccccccccccccccccccccsd sd s ds dd d d d d dnting and typesetting industry. Lortandard dummy text.'}
+                    />
+                    <Skill
+                        style={reactIconImg}
+                        title={'React'}
+                        description={'Le ptry. Lorem Ipsum has been the industry’s standard dummy text.'}
+                    />
+                    <Skill
+                        style={reduxIconImg}
+                        title={'Redux'}
+                        description={'Le ptry. Lorem Ipsum has been the industry’s standard dummy text.'}
+                    />
+                    <Skill
+                        style={typeScriptIc}
+                        title={'TypeScript'}
+                        description={'Lo printin been the industry’s standard dummy text.'}
                     />
                 </div>
             </div>
