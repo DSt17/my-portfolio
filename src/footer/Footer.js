cfd96aa64db25de95b531115c870e69265d7fede
DSt17 <dima3771770@gmail.com>
2022-04-27 13:04:29 +0300

    refactor style over sass and mixin

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,17 +1,41 @@
 import React from "react";
 import s from './Footer.module.scss'
 import styleContainer from "../common/styles/Container.module.css";
+import vkIcon from "../assets/img/vk-icon.png";
+import facebookIcon from "../assets/img/facebook-icon.png";
+import instagramIcon from "../assets/img/instagram-icon.png";
+import youtubeIcon from "../assets/img/youtube-icon.png";
+
 
 const Footer = () => {
+
     return (
         <div className={s.footerBlock}>
             <div className={`${styleContainer.container} + ${s.footerContainer}`}>
                 <span className={s.footerTitle}>Stoyanov Dmitry</span>
                 <div className={s.boxInFooterContainer}>
-                <div className={s.icon}></div>
-                <div className={s.icon}></div>
-                <div className={s.icon}></div>
-                <div className={s.icon}></div>
+                    <ul>
+                        <li>
+                            <a href={"https://www.vk.com/"} target={"_blank"}>
+                                <img src={vkIcon}/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href={"https://www.facebook.com/"} target={"_blank"}>
+                                <img src={facebookIcon}/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href={"https://www.instagram.com/"} target={"_blank"}>
+                                <img src={instagramIcon}/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href={"https://www.youtube.com/"} target={"_blank"}>
+                                <img src={youtubeIcon}/>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
                 <p className={s.footerBottomText}> © 2022 All rights reserved.</p>
             </div>
--- src/skills/Skill/Skill.js
+++ src/skills/Skill/Skill.js
@@ -5,8 +5,7 @@ import s from './Skill.module.scss'
 const Skill = (props) => {
     return (
         <div className={s.skill}>
-            <div className={s.icon} style={props.style}>
-            </div>
+            <div className={s.icon} style={props.style}></div>
             <h3>{props.title}</h3>
             <p>
                 {props.description}
